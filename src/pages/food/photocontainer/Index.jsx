50e12bb5ef3d9538e361a4a9a8d3665a0bd28c0d
BabamyratTKM <atajanowbabamurat@gmail.com>
2023-07-14 12:19:04 +0500

    Azamat ekäniň düzeden ütgeşmeleri

---
+++ src/pages/food/photocontainer/Index.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+function Index() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch(
+      "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=636e1481b4f3c446d26b8eb6ebfe7127&tags=food&per_page=24&format=json&nojsoncallback=1"
+    )
+      .then((response) => response.json())
+      .then((data) => setPosts(data.photos.photo));
+  }, []);
+  return (
+    <div className="Photo">
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <img
+              src={`https://farm66.staticflickr.com/65535/${post.id}_${post.secret}_m.jpg`}
+              alt=""
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Index;
